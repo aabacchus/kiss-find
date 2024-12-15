649fc9983d849cc5253e0c58760ec8809b59c978
phoebos <ben@bvnf.space>
2024-12-15 20:05:24 +0000

    cut instead of read

--- build_database.sh
+++ build_database.sh
@@ -49,7 +49,7 @@ process_repo() {
             VERSION="symlink"
             MAINTAINER="unknown"
         elif [ -f "$PACKAGE/version" ]; then
-            VERSION="$(read -r a b _ < "$PACKAGE/version"; printf "%s %s" "$a" "$b")"
+            VERSION="$(cut -d ' ' -f-2 "$PACKAGE/version")"
             MAINTAINER="$(git -C "$FOLDER" log --max-count 1 --format='%aN' -- "$PPATH/version" 2>/dev/null)"
         else
             VERSION="unknown"
